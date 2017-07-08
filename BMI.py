32c4e3b2628c417105dc8cf95647901e7bbc3a61
LiLiao <liliaowhu@foxmail.com>
2017-07-08 15:31:08 +0800

    BMI

---
+++ BMI.py
@@ -0,0 +1,15 @@
+height = input('height:')
+weight = input('weight:')
+height = float(height)
+weight = float(weight)
+BMI = weight/(height**2)
+if BMI<18.5:
+	print('过轻')
+elif 18.5<=BMI<25:
+	print('正常')
+elif 25<=BMI<28:
+	print('过重')
+elif 28<=BMI<=32:
+	print('肥胖')
+elif BMI>32:
+	print('严重肥胖')
